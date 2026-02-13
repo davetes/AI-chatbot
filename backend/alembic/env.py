5a51bd209593eb6ca7b3a28a835dd7dfd9342d42
davetes <tesfa4706@gmail.com>
2026-02-13 14:51:06 +0300

    fix alembic

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,7 +1,13 @@
 from logging.config import fileConfig
 import os
+import sys
+from pathlib import Path
 from alembic import context
 from sqlalchemy import engine_from_config, pool
+
+ROOT = Path(__file__).resolve().parents[1]
+if str(ROOT) not in sys.path:
+    sys.path.insert(0, str(ROOT))
 
 from app.models.db import Base
 
